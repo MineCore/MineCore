ac294dcf17f780e9a0145d33682e8363b8157a5d
Francesco Macagno <fjmacagno1@yahoo.com>
2014-01-13 11:14:32 -0800

    Updated Metrics

--- src/net/minecore/MineCore.java
+++ src/net/minecore/MineCore.java
@@ -17,44 +17,42 @@ public class MineCore extends JavaPlugin {
 	private File playerFolder;
 	private EconomyManager em;
 	private FileConfiguration conf;
+	private Metrics metrics;
 
 	@Override
 	public void onLoad() {
-		
-		
+
 		log = this.getLogger();
 		mm = new MinerManager(this);
-		
+
 		conf = this.getConfig();
 		conf.options().copyDefaults(true);
-		
+
 		conf.addDefault("useVaultEcon", false);
 		conf.addDefault("currency_item", 371);
-		
+		conf.addDefault("allowDataCollection", true);
 
-		playerFolder = new File(this.getDataFolder().getPath() + File.separator
-				+ "players");
+		playerFolder = new File(this.getDataFolder().getPath() + File.separator + "players");
 		playerFolder.mkdirs();
 
 		for (File f : playerFolder.listFiles())
 			if (f.getName().endsWith(".yml")) {
 				try {
-					mm.addMiner(Miner.loadMinerFromYamlFile(f.getName()
-							.substring(0, f.getName().indexOf('.')), f));
+					mm.addMiner(Miner.loadMinerFromYamlFile(
+							f.getName().substring(0, f.getName().indexOf('.')), f));
 				} catch (IOException e) {
-					log.warning("Encountered error while loading player file "
-							+ f.getPath() + ": " + e.getMessage());
-				} catch (InvalidConfigurationException e) {
-					log.warning("Invalid player file " + f.getPath() + ": "
+					log.warning("Encountered error while loading player file " + f.getPath() + ": "
 							+ e.getMessage());
+				} catch (InvalidConfigurationException e) {
+					log.warning("Invalid player file " + f.getPath() + ": " + e.getMessage());
 				}
 			}
-		
+
 		saveConf();
 
 	}
-	
-	public void saveConf(){
+
+	public void saveConf() {
 		try {
 			conf.save(new File(this.getDataFolder().getPath() + File.separator + "config.yml"));
 		} catch (IOException e1) {
@@ -64,17 +62,19 @@ public class MineCore extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		
+
 		boolean useVaultEcon = conf.getBoolean("useVaultEcon");
 		Material currencyItem = Material.matchMaterial(conf.getString("currency_item"));
-		
+
 		em = new EconomyManager(useVaultEcon, currencyItem, this);
-		
-		try {
-			Metrics metrics = new Metrics(this);
-			metrics.start();
-		} catch (IOException e1) {
-			e1.printStackTrace();
+
+		if (conf.getBoolean("allowDataCollection")) {
+			try {
+				metrics = new Metrics(this);
+				metrics.start();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 		}
 	}
 
@@ -83,8 +83,8 @@ public class MineCore extends JavaPlugin {
 		for (Miner m : mm.getMiners().values())
 			try {
 				log.info("Saving player " + m.getPlayerName());
-				m.saveMinerToYamlFile(new File(playerFolder.getPath()
-						+ File.separator + m.getPlayerName() + ".yml"));
+				m.saveMinerToYamlFile(new File(playerFolder.getPath() + File.separator
+						+ m.getPlayerName() + ".yml"));
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -104,8 +104,8 @@ public class MineCore extends JavaPlugin {
 	public Miner getMiner(String name) {
 		return mm.getMiner(name);
 	}
-	
-	public EconomyManager getEconomyManager(){
+
+	public EconomyManager getEconomyManager() {
 		return em;
 	}
 
